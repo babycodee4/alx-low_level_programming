50716fd5ff3522b90cd0aa64b7bc75015bf43ecc
babycodee4 <dennisolive@gmail.com>
2022-09-08 15:51:01 -0700

    hard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <studio.h>
 
 /**
@@ -10,6 +9,7 @@
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("/"Programming is like building a multilingual puzzle");
+        return (0);
 }
+
