cad03a1761473fa2e777ba92b6b2bb1a512d59f7
babycodee4 <dennisolive@gmail.com>
2022-09-12 04:33:08 -0700

    trend

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - Prints out the last part of a quote in the standard error.
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 		return (1);
 }
