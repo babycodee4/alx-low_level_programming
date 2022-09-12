8639ac25e1abd136068e1d2ae2620562829105df
babycodee4 <dennisolive@gmail.com>
2022-09-12 05:46:33 -0700

    Afrofusion

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,18 +2,20 @@
 
 /**
  * main - print the number if it is positive, zero, or negative
- *
  * Description: using the main function
  * This program prints "Programming is positive, zero, or negative
- * Retun: 0
+ * Retun: Always (Success)
  */
 int main(void)
 {
 	int n;
-	for (n = '0'; n <= '9'; n++)
+
+	for(n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
+
 	}
+	
 	putchar('\n');
 
 	return (0);
