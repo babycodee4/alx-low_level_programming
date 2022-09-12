cee0b35631998020c8fbd817aa4e3fa8677be420
babycodee4 <dennisolive@gmail.com>
2022-09-12 05:29:50 -0700

    Afrofusion

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the number if it is positive, zero, or negative
+ *
+ * Description: using the main function
+ * This program prints "Programming is positive, zero, or negative
+ * Retun: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n<= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0;);
+}
