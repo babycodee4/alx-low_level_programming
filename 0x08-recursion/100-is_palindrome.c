ea5994d66057f54a624389da5cd309d26c65ed30
babycodee4 <dennisolive@gmail.com>
2022-09-28 15:32:45 -0700

    von

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,54 +1,42 @@
 #include "main.h"
-
 /**
- * is_palindrome - returns the 1 if s is a palindrome
- * @s: string to be checked
- *
- * Return: 1 if s is a palindrome, 0 otherwise
+ * get_length - gets length of string
+ * @str: pointer to a string
+ * @len: length of a string
+ * Return: return length of string
+ */
+int get_length(char *str, int len)
+{
+	if (*(str + len) != '\0')
+	{
+		return (get_length(str, ++len));
+	}
+	return (--len);
+}
+/**
+ * check_pali - gets length of string
+ * @f: pointer to a string
+ * @r: length of a string
+ * @len: length of a string
+ * Return: return 0 if its not palindrome return 1 if it is
+ */
+int check_pali(char *f, char *r, int len)
+{
+	if (*f == *(r + len) && len > 1)
+	{
+		f++;
+		return (check_pali(f, r, --len));
+	}
+	if (len == 1)
+		return (1);
+	return (0);
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: pointer to a string
+ * Return: return 0 if its not palindrome return 1 if it is
  */
 int is_palindrome(char *s)
 {
-	int flag = 1;
-
-	check(s, 0, _strlen_recursion(s) - 1, &flag);
-	return (flag);
-}
-
-/**
- * check - checks if a string is a palindrome
- * @s: string to be checked
- * @start: start index
- * @end: end index
- * @flag: flag to indicate if a string is a palindrome
- *
- * Return: void
- */
-void check(char *s, int start, int end, int *flag)
-{
-	if (start <= end)
-	{
-		if (s[start] == s[end])
-			*flag *= 1;
-		else
-			*flag *= 0;
-		check(s, start + 1, end - 1, flag);
-	}
-}
-
-/**
- * _strlen_recursion - calculates the length of a string
- * @s: string to be used
- *
- * Return: length of the string
- */
-int _strlen_recursion(char *s)
-{
-	int sum = 0;
-
-	if (*s != '\0')
-	{
-		sum++;
-		sum += _strlen_recursion(s + 1);
-	}
-	return (sum);
+	return (check_pali(s, s, get_length(s, 0)));
 }
