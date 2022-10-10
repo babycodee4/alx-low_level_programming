bb69930d7e27d21cd23a258d3033d32dfdcc2316
babycodee4 <dennisolive@gmail.com>
2022-10-10 14:15:48 -0700

    fred

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,4 +1,3 @@
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,4 +17,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *hays
+char *_strstr(char *haystack, char *needle);
